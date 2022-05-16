90960cf61b17e4af79161f3e66c5ff6721df2dec
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-05-16 18:02:02 -0300

    Add state wrappers

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import AuthProvider from '../context/auth/AuthContext'
+import AppProvider from '../context/app/AppContext'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <AppProvider>
+        <Component {...pageProps} />
+      </AppProvider>
+    </AuthProvider>
+  )
 }
 
 export default MyApp

807cdd59f6420ae97254059f2351cbdaf0a629d4
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-05-16 18:15:22 -0300

    Create report history component

---
+++ components/ReportsHistory/index.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react'
+import styles from './index.module.css'
+import ActivityIndicator from '../ActivityIndicator'
+import useApp from '../../hooks/useApp'
+import Report from './ReportItem'
+
+export default function ReportsHistory() {
+  const [loading, setLoading] = useState(false)
+  const { reports = [], getReports } = useApp()
+
+  useEffect(() => {
+    setLoading(true)
+    getReports().finally(() => setLoading(false))
+  }, [])
+
+  return (
+    <div className={styles.reportsHistory}>
+      {loading && <ActivityIndicator />}
+      {!loading && reports.length > 0 && (
+        <ul className={styles.reports}>
+          {reports.map(report => (
+            <Report key={report._id} report={report} />
+          ))}
+        </ul>
+      )}
+      {!loading && reports.length === 0 && <p>No reports yet...</p>}
+    </div>
+  )
+}

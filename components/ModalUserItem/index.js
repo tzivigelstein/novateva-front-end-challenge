10161d9b026202bd60f4ba7dd7cc2002cb8d07fb
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-05-16 18:16:21 -0300

    Create modal user item component

---
+++ components/ModalUserItem/index.js
@@ -0,0 +1,26 @@
+import useApp from '../../hooks/useApp'
+import { ArrowRight, Check } from '../Icons'
+import UserInfo from '../UserInfo'
+import styles from './index.module.css'
+
+export default function UserItem({ user }) {
+  const { selectedUserId, setSelectedUser } = useApp()
+  const { _id: userId } = user
+
+  const isSelected = selectedUserId === userId
+
+  return (
+    <li className={styles.userItem}>
+      <button aria-selected={isSelected} className={styles.userItemButton} onClick={() => setSelectedUser(userId)}>
+        <UserInfo user={user} bold={true} />
+        <div className={styles.userSelectButton}>
+          {isSelected ? (
+            <Check className={styles.userSelectedButtonIcon} />
+          ) : (
+            <ArrowRight className={styles.userSelectButtonIcon} />
+          )}
+        </div>
+      </button>
+    </li>
+  )
+}

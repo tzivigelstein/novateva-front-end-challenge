01513f01a177e6f9b15037f7598f86387d9de11b
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-05-17 16:26:21 -0300

    Fix oneliners

--- config/axiosClient.js
+++ config/axiosClient.js
@@ -4,9 +4,12 @@ const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_BASE_URL
 
 const isWindowDefined = typeof window !== 'undefined'
 
+const localToken = isWindowDefined && window.localStorage.getItem('token')
+const token = isWindowDefined ? `Bearer ${localToken}` : ''
+
 export default axios.create({
   baseURL: BASE_URL,
   headers: {
-    Authorization: isWindowDefined ? `Bearer ${window.localStorage.getItem('token')}` : ''
+    Authorization: token
   }
 })

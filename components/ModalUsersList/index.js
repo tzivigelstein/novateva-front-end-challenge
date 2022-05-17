f1c58195428361ff309eb8bc539cae75dadb6cbd
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-05-17 16:25:25 -0300

    Remove local user filtering

--- components/ModalUsersList/index.js
+++ components/ModalUsersList/index.js
@@ -3,10 +3,8 @@ import styles from './index.module.css'
 import useApp from '../../hooks/useApp'
 import ActivityIndicator from '../ActivityIndicator'
 import UserItem from '../ModalUserItem'
-import useAuth from '../../hooks/useAuth'
 
 export default function ModalUsersList() {
-  const { user: localUser } = useAuth()
   const { users, getAllUsers } = useApp()
   const [loading, setLoading] = useState(false)
 
@@ -19,11 +17,9 @@ export default function ModalUsersList() {
     <div className={styles.modalUserListContainer}>
       {users.length > 0 ? (
         <ul className={styles.usersList}>
-          {users
-            .filter(user => user._id !== localUser._id)
-            .map(user => (
-              <UserItem key={user._id} user={user} />
-            ))}
+          {users.map(user => (
+            <UserItem key={user._id} user={user} />
+          ))}
         </ul>
       ) : (
         <>{loading ? <ActivityIndicator /> : <p>It seems that there are no users...</p>}</>

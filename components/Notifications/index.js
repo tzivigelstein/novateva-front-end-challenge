942ffba23b31a3d4e311839c26c3da9ade63e20e
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-05-17 16:25:44 -0300

    Add notification count

--- components/Notifications/index.js
+++ components/Notifications/index.js
@@ -1,13 +1,19 @@
+import { useMemo } from 'react'
+import styles from './index.module.css'
 import NotificationsCount from '../NotificationsCount'
 import { Bell } from '../Icons'
-import styles from './index.module.css'
+import useApp from '../../hooks/useApp'
 
 export default function Notifications() {
+  const { getAllUnreadNotifications, chats } = useApp()
+
+  const count = useMemo(() => getAllUnreadNotifications(), [chats])
+
   return (
     <button className={styles.notifications}>
       <Bell />
       <div className={styles.notificationsCountContainer}>
-        <NotificationsCount number={24} />
+        <NotificationsCount number={count} />
       </div>
     </button>
   )
--- components/SideBar/Identifier/index.js
+++ components/SideBar/Identifier/index.js
@@ -1,14 +1,20 @@
-import NotificationsCount from '../NotificationCount'
+import { useMemo } from 'react'
 import styles from './index.module.css'
+import useApp from '../../../hooks/useApp'
+import NotificationsCount from '../NotificationCount'
 
 export default function Identifier({ name, icon }) {
+  const { getAllUnreadNotifications, chats } = useApp()
+
+  const count = useMemo(() => getAllUnreadNotifications(), [chats])
+
   return (
     <div className={styles.chatIdentifier}>
       <div className={styles.identifier}>
         {icon}
         <span className={styles.identifierName}>{name}</span>
       </div>
-      <NotificationsCount className={styles.notificationsCount} number={19} />
+      <NotificationsCount className={styles.notificationsCount} number={count} />
     </div>
   )
 }

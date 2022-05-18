5a6515b6c457d37d7e84746d94ebd4afbf18ea5a
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-05-18 11:23:45 -0300

    Add search bar functionality and debounce

--- components/SearchBar/index.js
+++ components/SearchBar/index.js
@@ -1,13 +1,88 @@
+import { useState, useMemo } from 'react'
 import styles from './index.module.css'
 import { Search } from '../Icons'
+import { debounce } from './utils'
+import axiosClient from '../../config/axiosClient'
+
+const SEARCH_AFTER_500_MS = 500
 
 export default function SearchBar() {
+  const debounced = useMemo(() => debounce(handleSearch, SEARCH_AFTER_500_MS), [])
+  const [search, setSearch] = useState('')
+  const [filteredUsers, setFilteredUsers] = useState([])
+  const [isBlured, setIsBlured] = useState(false)
+  const [isFocused, setIsFocused] = useState(false)
+
+  function handleSearch({ value }) {
+    const ALL = '*'
+
+    axiosClient('/users').then(data => {
+      const { users } = data.data
+
+      const filteredUsers = users.filter(user => {
+        const { firstName, lastName } = user
+        if (value === ALL) {
+          return true
+        }
+
+        return (
+          firstName.toLowerCase().includes(value.toLowerCase()) ||
+          lastName.toLowerCase().includes(value.toLowerCase()) ||
+          `${firstName} ${lastName}`.toLowerCase().includes(value.toLowerCase())
+        )
+      })
+
+      if (users.length === filteredUsers.length && value !== ALL) {
+        setFilteredUsers([])
+      } else {
+        setFilteredUsers(filteredUsers)
+      }
+    })
+  }
+
+  const handleInputChange = ({ target: { value } }) => {
+    const parsedValue = value.trim()
+
+    setSearch(value)
+    if (parsedValue === '' && filteredUsers.length === 0) return
+    debounced({ value: parsedValue })
+  }
+
+  function handleBlur() {
+    setIsFocused(false)
+    setIsBlured(true)
+  }
+
+  function handleFocus() {
+    setIsFocused(true)
+    setIsBlured(false)
+  }
+
   return (
-    <div className={styles.inputContainer}>
-      <label htmlFor="search">
-        <Search />
-      </label>
-      <input className={styles.input} id="search" placeholder="User search" />
+    <div style={{ position: 'relative' }}>
+      <div className={styles.inputContainer}>
+        <label htmlFor="search">
+          <Search />
+        </label>
+        <input
+          onChange={handleInputChange}
+          onBlur={handleBlur}
+          onFocus={handleFocus}
+          className={styles.input}
+          value={search}
+          id="search"
+          placeholder="User search"
+        />
+      </div>
+      {filteredUsers.length > 0 && isFocused && (
+        <ul className={styles.resultsList}>
+          {filteredUsers.map(({ firstName, lastName, _id }, index) => (
+            <li className={styles.listItem} key={_id}>
+              <span className={styles.userIndex}>{index + 1}</span> {firstName} {lastName}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }

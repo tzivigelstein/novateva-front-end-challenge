cd7d1f6da5d5eb3db91a7cc67f2a6d2df1b4452f
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-05-18 20:37:14 -0300

    Fix crossOrigin value

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ class MyDocument extends Document {
         <Head>
           <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;0,700;1,400&display=swap"
             rel="stylesheet"

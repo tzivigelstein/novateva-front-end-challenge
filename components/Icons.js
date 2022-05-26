d1536293e55b22af31db05b3b461bde2e92670d6
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-05-26 14:39:56 -0300

    Add user icon component

--- components/Icons.js
+++ components/Icons.js
@@ -1,3 +1,20 @@
+export const User = props => (
+  <svg
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+    stroke="currentColor"
+    strokeWidth="2"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+    <circle cx="12" cy="7" r="4"></circle>
+  </svg>
+)
+
 export const Search = props => (
   <svg
     viewBox="0 0 24 24"

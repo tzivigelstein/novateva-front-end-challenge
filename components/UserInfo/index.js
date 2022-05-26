125243c642ed750f64565c29c8743d9559a7505a
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-05-26 14:40:51 -0300

    Remove unused import and pass user prop

--- components/UserInfo/index.js
+++ components/UserInfo/index.js
@@ -1,4 +1,3 @@
-import useAuth from '../../hooks/useAuth'
 import UserName from '../UserName'
 import UserPicture from '../UserPicture'
 import styles from './index.module.css'
@@ -6,7 +5,7 @@ import styles from './index.module.css'
 export default function UserInfo({ user, bold, status }) {
   return (
     <div className={styles.userInfoContainer}>
-      <UserPicture status={status} />
+      <UserPicture user={user} status={status} />
       <div className={styles.info}>
         <UserName bold={bold} name={`${user?.firstName ?? 'Deleted'} ${user?.lastName ?? 'user'}`} />
         {status && <span className={styles.status}>Online</span>}

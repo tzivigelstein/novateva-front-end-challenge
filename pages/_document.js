9abb56ed9b78979fc1492a669d5a321b5cb8e521
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-05-30 10:08:19 -0300

    Add app icon

--- pages/_document.js
+++ pages/_document.js
@@ -7,13 +7,13 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;0,700;1,400&display=swap"
             rel="stylesheet"
           />
+          <link rel="shortcut icon" href="/favicon.svg" type="image/svg" />
         </Head>
         <body>
           <Main />

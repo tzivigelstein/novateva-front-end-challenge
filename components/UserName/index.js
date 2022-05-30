09b4510644bee986f11feb8d5d89f5ea0815a7ba
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-05-30 10:29:46 -0300

    Add optional short id display

--- components/UserName/index.js
+++ components/UserName/index.js
@@ -1,9 +1,10 @@
 import styles from './index.module.css'
 
-export default function UserName({ name, bold }) {
+export default function UserName({ id, name, bold }) {
   return (
     <span style={bold ? { fontWeight: 'bold' } : {}} className={styles.name}>
       {name}
+      {id && <span className={styles.userId}>#{id?.slice(0, 4)}</span>}
     </span>
   )
 }

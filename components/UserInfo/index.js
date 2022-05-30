456e51a58920ece21b3f86719bd270a8b01fbe43
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-05-30 10:30:14 -0300

    Create optional id display

--- components/UserInfo/index.js
+++ components/UserInfo/index.js
@@ -2,12 +2,16 @@ import UserName from '../UserName'
 import UserPicture from '../UserPicture'
 import styles from './index.module.css'
 
-export default function UserInfo({ user, bold, status }) {
+export default function UserInfo({ user, bold, status, showId }) {
   return (
     <div className={styles.userInfoContainer}>
       <UserPicture user={user} status={status} />
       <div className={styles.info}>
-        <UserName bold={bold} name={`${user?.firstName ?? 'Deleted'} ${user?.lastName ?? 'user'}`} />
+        <UserName
+          bold={bold}
+          name={`${user?.firstName ?? 'Deleted'} ${user?.lastName ?? 'user'}`}
+          id={showId ? user?._id : null}
+        />
         {status && <span className={styles.status}>Online</span>}
       </div>
     </div>

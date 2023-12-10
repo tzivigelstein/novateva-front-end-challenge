69a229ec71d397f3496db770560d1b77829337e8
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-12-10 14:45:50 -0300

    fix NaN error

--- components/NotificationsCount/index.js
+++ components/NotificationsCount/index.js
@@ -3,7 +3,7 @@ import styles from './index.module.css'
 export default function NotificationsCount({ number = 0 }) {
   return (
     <div className={styles.notificationsCountContainer}>
-      <span data-testid="notification-count" className={styles.notificationsCount}>{number}</span>
+      <span data-testid="notification-count" className={styles.notificationsCount}>{Number.isNaN(number) ? 0 : number}</span>
     </div>
   )
 }
--- components/SideBar/NotificationCount/index.js
+++ components/SideBar/NotificationCount/index.js
@@ -3,7 +3,7 @@ import styles from './index.module.css'
 export default function NotificationsCount({ number = 0 }) {
   return (
     <div className={styles.notificationsCountContainer}>
-      <span className={styles.notificationsCount}>{number}</span>
+      <span className={styles.notificationsCount}>{Number.isNaN(number) ? 0 : number}</span>
     </div>
   )
 }
